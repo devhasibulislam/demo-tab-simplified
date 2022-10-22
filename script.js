ff0649f6279e5e616261d47f36b8e5ca301ba615
hasibulislam999 <hasib143sl@gmail.com>
2022-10-22 23:07:25 +0600

    script js modified

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const layout3 = document.querySelector(".layout3-boundary");
 const LayoutSwitchingButton = document.querySelectorAll(".SwitchLayout");
 for (let i = 0; i < LayoutSwitchingButton.length; i++) {
   layout1.classList.remove("hidden");
+  LayoutSwitchingButton[0].classList.add("active");
   if (LayoutSwitchingButton[0]) {
     LayoutSwitchingButton[0].addEventListener("click", function () {
+      LayoutSwitchingButton[0].classList.add("active");
+      LayoutSwitchingButton[1].classList.remove("active");
+      LayoutSwitchingButton[2].classList.remove("active");
       layout1.classList.remove("hidden");
       layout2.classList.add("hidden");
       layout3.classList.add("hidden");
@@ -13,6 +17,9 @@ for (let i = 0; i < LayoutSwitchingButton.length; i++) {
   }
   if (LayoutSwitchingButton[1]) {
     LayoutSwitchingButton[1].addEventListener("click", function () {
+      LayoutSwitchingButton[1].classList.add("active");
+      LayoutSwitchingButton[0].classList.remove("active");
+      LayoutSwitchingButton[2].classList.remove("active");
       layout2.classList.remove("hidden");
       layout1.classList.remove("layout1-boundary");
       layout1.classList.add("hidden");
@@ -21,6 +28,9 @@ for (let i = 0; i < LayoutSwitchingButton.length; i++) {
   }
   if (LayoutSwitchingButton[2]) {
     LayoutSwitchingButton[2].addEventListener("click", function () {
+      LayoutSwitchingButton[2].classList.add("active");
+      LayoutSwitchingButton[0].classList.remove("active");
+      LayoutSwitchingButton[1].classList.remove("active");
       layout3.classList.remove("hidden");
       layout1.classList.remove("layout1-boundary");
       layout1.classList.add("hidden");
